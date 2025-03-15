a4ac4dfe676d24db658c29a861532149e4f997f2
User <asphaltlev@gmail.com>
2025-03-15 03:05:44 +0200

    Implemented class "TemperatureArray"

--- book_list/Program.cs
+++ book_list/Program.cs
@@ -1,9 +1,11 @@
 ﻿using book_lst;
+using MyNamespace;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        // 1
         book_list obj = new book_list();
         obj = obj + "hello";
         obj = obj + "world";
@@ -14,5 +16,16 @@ internal class Program
         obj.print();
 
         Console.WriteLine(obj == "world");
+        Console.WriteLine();
+
+        // 2
+        TemperatureArray array = new TemperatureArray(15.5, 17.2, 20.1, 22.0, 18.4, 16.8, 14.3);
+        array.Show();
+        
+        Console.WriteLine(array[0]);
+        
+        array[0] = 25.0;
+        array.Show();
+        Console.WriteLine(array.Average());
     }
 }
---
+++ book_list/TemperatureArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNamespace
+{
+    internal class TemperatureArray
+    {
+        private double[] array;
+
+        public TemperatureArray(double mon, double tue, double wed, double thu, double fri, double sat, double sun)
+            => array = new double[] { mon, tue, wed, thu, fri, sat, sun };
+
+        public double this[int index]
+        {
+            get => array[index];
+            set => array[index] = value;
+        }
+
+        public double Average()
+        {
+            return Math.Round((array.Sum() / array.Length), 2);
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
