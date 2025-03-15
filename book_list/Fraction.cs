55fefb5f5c41f8b6c6d490e056eabf9b9a9dce6d
User <asphaltlev@gmail.com>
2025-03-15 03:42:50 +0200

    class "Fracture" implemented

---
+++ book_list/Fraction.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using book_lst;
+
+namespace MyNamespace2
+{
+    internal class Fraction
+    {
+        public double numerator; // числитель
+        public double denominator; // знаменатель
+
+        public Fraction(double num, double den)
+        {
+            if (den == 0) { throw new ArgumentException("The denominator cannot be zero."); }
+
+            numerator = num;
+            denominator = den;
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            double D = a.denominator * b.denominator;
+            double N = a.numerator * b.denominator + b.numerator * a.denominator;
+
+            return new Fraction(N, D);
+        }
+
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            double D = a.denominator * b.denominator;
+            double N = a.numerator * b.denominator - b.numerator * a.denominator;
+
+            return new Fraction(N, D);
+        }
+
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            double D = a.denominator * b.denominator;
+            double N = a.numerator * b.numerator;
+
+            return new Fraction(N, D);
+        }
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            double D = a.numerator * b.denominator;
+            double N = a.denominator * b.numerator;
+
+            return new Fraction(N, D);
+        }
+
+        public static bool operator ==(Fraction a, Fraction b)
+        {
+            return (a.numerator * b.denominator == b.numerator * a.denominator);
+        }
+
+        public static bool operator !=(Fraction a, Fraction b)
+        {
+            return !(a == b);
+        }
+
+        public override string ToString()
+        {
+            return $"{numerator}/{denominator}";
+        }
+    }
+}
--- book_list/Program.cs
+++ book_list/Program.cs
@@ -1,5 +1,6 @@
 ﻿using book_lst;
 using MyNamespace;
+using MyNamespace2;
 
 internal class Program
 {
@@ -27,5 +28,17 @@ internal class Program
         array[0] = 25.0;
         array.Show();
         Console.WriteLine(array.Average());
+        Console.WriteLine();
+
+        // 3
+        Fraction f1 = new Fraction(1, 2);
+        Fraction f2 = new Fraction(1, 3);
+        
+        Fraction result = f1 + f2;
+        Console.WriteLine(result);
+
+        Fraction result1 = f1 - f2;
+        Console.WriteLine(result1);
+        Console.WriteLine(f1 == f2);
     }
 }
